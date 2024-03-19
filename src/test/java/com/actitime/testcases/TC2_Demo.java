c0bb53c45728f3668123c8d44985477e95cc9725
NehaNo1 <patroankita12@gmail.com>
2024-03-19 10:27:34 +0530

    new test case added

---
+++ src/test/java/com/actitime/testcases/TC2_Demo.java
@@ -0,0 +1,13 @@
+package com.actitime.testcases;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC2_Demo 
+{
+	@Test
+	public void demoTestCase()
+	{
+		Reporter.log("demo", true);
+	}
+}
